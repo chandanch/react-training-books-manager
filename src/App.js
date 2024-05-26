466900a380be73da50c61268ef9df99139e5f8b6
chandanch <chandan.cec107@gmail.com>
2024-05-26 21:28:05 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { useState } from "react";
-import axios from "axios";
-import BookCreate from "./components/BookCreate";
-import BookList from "./components/BookList";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
+    const response = await axios.get('http://localhost:3001/books');
 
     setBooks(response.data);
   };
 
-  // DON'T
-  // fetchBooks();
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -36,7 +37,7 @@ function App() {
   };
 
   const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
+    const response = await axios.post('http://localhost:3001/books', {
       title,
     });
 
